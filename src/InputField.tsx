711476ad9df7ae17a27fdcae3b79c7d989e0fcde
Iulian Radu <iulian.radu@gmx.at>
2019-02-08 20:19:28 +0100

    We add a name automatically to the input field so the browser can keep a history for it

--- src/InputField.tsx
+++ src/InputField.tsx
@@ -1,58 +1,59 @@
 import * as React from 'react';
-import {isEmpty} from 'lodash';
+import { isEmpty, kebabCase } from 'lodash';
 import FormControl from '@material-ui/core/FormControl/FormControl';
 import InputLabel from '@material-ui/core/InputLabel/InputLabel';
 import Input from '@material-ui/core/Input/Input';
 import FormHelperText from '@material-ui/core/FormHelperText/FormHelperText';
 
-const getType = (type?: string): string => isEmpty(type) ? 'text' : type as string;
+const getType = (type?: string): string => (isEmpty(type) ? 'text' : type as string);
 
 const getLabel = (id?: string, label?: string) => {
-    if (isEmpty(label)) {
-        return null;
-    }
+	if (isEmpty(label)) {
+		return null;
+	}
 
-    return <InputLabel htmlFor={id}>{label}</InputLabel>;
+	return <InputLabel htmlFor={id}>{label}</InputLabel>;
 };
 
 const getHelperText = (helperText: React.ReactNode) => {
-    if (isEmpty(helperText)) {
-        return null;
-    }
+	if (isEmpty(helperText)) {
+		return null;
+	}
 
-    return <FormHelperText>{helperText}</FormHelperText>;
+	return <FormHelperText>{helperText}</FormHelperText>;
 };
 
 const InputField = (props: InputFieldProps) => (
-    <FormControl fullWidth={true}>
-        {getLabel(props.id, props.label)}
-        <Input
-            id={props.id}
-            type={getType(props.type)}
-            placeholder={props.placeholder}
-            value={props.value}
-            onFocus={props.onGotFocus}
-            onBlur={props.onLostFocus}
-            onChange={props.onChange}
-            startAdornment={props.startAdornment}
-            endAdornment={props.endAdornment}
-        />
-        {getHelperText(props.helperText)}
-    </FormControl>
+	<FormControl fullWidth={true}>
+		{getLabel(props.id, props.label)}
+		<Input
+			id={props.id}
+			type={getType(props.type)}
+			placeholder={props.placeholder}
+			value={props.value}
+			name={kebabCase(props.id || props.label || props.placeholder)}
+			onFocus={props.onGotFocus}
+			onBlur={props.onLostFocus}
+			onChange={props.onChange}
+			startAdornment={props.startAdornment}
+			endAdornment={props.endAdornment}
+		/>
+		{getHelperText(props.helperText)}
+	</FormControl>
 );
 
 export interface InputFieldProps {
-    id?: string;
-    type: string;
-    label?: string;
-    placeholder?: string;
-    value?: string;
-    onGotFocus?: () => void;
-    onLostFocus?: () => void;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    startAdornment?: React.ReactNode;
-    endAdornment?: React.ReactNode;
-    helperText?: string;
+	id?: string;
+	type: string;
+	label?: string;
+	placeholder?: string;
+	value?: string;
+	onGotFocus?: () => void;
+	onLostFocus?: () => void;
+	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	startAdornment?: React.ReactNode;
+	endAdornment?: React.ReactNode;
+	helperText?: string;
 }
 
 export default InputField;
